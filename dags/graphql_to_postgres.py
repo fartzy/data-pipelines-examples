d560eca9cb1fd98216b5190a7646e1defbe22b87
Mike Artz <fartzy@hotmail.com>
2020-09-24 15:24:39 -0700

    Adding DAGS

---
+++ dags/graphql_to_postgres.py
@@ -0,0 +1,167 @@
+import json
+import logging
+import os
+from datetime import datetime
+
+import psycopg2
+import psycopg2.extras
+from airflow import DAG
+from airflow import configuration as conf
+from airflow.operators.python_operator import PythonOperator
+from airflow.operators.latest_only_operator import LatestOnlyOperator
+from airflow.operators.postgres_operator import PostgresOperator
+from airflow.hooks.base_hook import BaseHook
+from python_graphql_client import GraphqlClient
+
+from dags.utils import demodb
+
+ARGS = {
+    "description": "graphql__teams",
+    "owner": "user@email.com",
+    "depends_on_past": False,
+    "email": ["martz@email.com"],
+    "email_on_failure": False,
+    "email_on_retry": False,
+    "retries": 0,
+}
+
+dag_name = "graphql__teams_headcount"
+
+dag = DAG(
+    dag_name,
+    default_args=ARGS,
+    start_date=datetime(2020, 1, 1),
+    catchup=False,
+    schedule_interval="@monthly",
+)
+
+
+def main():
+
+    demo_DATABASE_SCHEMA = "raw"
+    demo_DATABASE_TABLE = "graphql__teams_headcount"
+
+    # GraphQL credential setup
+    demo_GRAPHQL_API_CREDENTIALS = BaseHook.get_connection("demo_graphql_api_token")
+    GRAPHQL_HEADERS = {
+        "Authorization": f"Bearer {demo_GRAPHQL_API_CREDENTIALS.password}",
+        "Content-type": "application/json",
+    }
+    GRAPHQL_BASE_URL = f"https://{demo_GRAPHQL_API_CREDENTIALS.host}"
+
+    # Instantiate the client with the Riot GraphQL endpoint
+    client = GraphqlClient(endpoint=f"{GRAPHQL_BASE_URL}/api/v1/graphql", headers=GRAPHQL_HEADERS)
+
+    # graphQL query for the request to the above api endpoint
+    query = """
+        {
+            group
+            {
+                team_id : scope
+                headcount
+            }
+        } 
+    """
+
+    # Synchronous request
+    res = client.execute(query=query,)["data"]["group"]
+
+    # prep list of dicts for pscycop insert
+    insert_vals = [(tm["team_id"], tm["headcount"],) for tm in res]
+
+    # Open a connection to the demo database
+    with demodb.get_connection() as demodb_connection:
+        with demodb_connection.cursor() as cur:
+            args = ",".join(cur.mogrify("(%s,%s)", x).decode("utf8") for x in insert_vals)
+
+            cur.execute(
+                f"""
+                  insert into {demo_DATABASE_SCHEMA}.{demo_DATABASE_TABLE} (team_id, name) values 
+                  {args}
+                """
+            )
+
+
+# Operator/Task Declaration
+latest_only = LatestOnlyOperator(task_id="latest_only", dag=dag)
+create_tables = PostgresOperator(
+    task_id="graphql__teams_headcount_create_tables",
+    postgres_conn_id="demo_data_db",
+    dag=dag,
+    auto_commit=True,
+    sql=[
+        """
+        create table if not exists raw.graphql__teams_headcount (
+            headcount int
+          , team_id varchar(200)
+          , loaded_to_conformed boolean default false
+          , insertion_datetime timestamptz default now()
+        )      
+      """,
+        """
+        create table if not exists conform.graphql__teams_headcount (
+            id serial primary key
+          , headcount int
+          , team_id varchar(200) references conform.graphql__teams(id)
+          , active boolean default true
+          , insertion_datetime timestamptz default now()
+          , unique(team_id, insertion_datetime)
+        )      
+      """,
+    ],
+)
+trasform_to_conformed = PostgresOperator(
+    task_id="conform.graphql__teams_headcount",
+    postgres_conn_id="demo_data_db",
+    auto_commit=True,
+    sql=[
+        """
+      with latest as (
+        select * 
+        from raw.graphql__teams_headcount
+        where loaded_to_conformed = false
+      )
+
+      select headcount
+        , team_id
+      into temp temp_graphql__teams_headcount
+      from latest 
+    """,
+        """
+      insert into conform.graphql__teams_headcount (headcount, team_id) 
+      select id
+        , words
+      from temp_graphql__teams_headcount thc
+      where not exists (
+        select 1
+        from conform.graphql__teams_headcount cgth
+        where cgth.team_id = thc.team_id
+          and cgth.headcount = thc.headcount
+          and cgth.active is true
+        )
+    """,
+        """
+      with update_headcount as (
+        select rank() over (partition by team_id order by insertion_datetime desc) as recent_rank
+          , team_id
+          , insertion_datetime
+          , active
+        from conform.graphql__teams_headcount
+      )
+
+      update conform.graphql__teams_headcount cth
+      set active = false 
+      from update_headcount uh 
+      where uh.team_id = cth.team_id
+        and uh.insertion_datetime = cth.insertion_datetime
+        and uh.recent_rank > 1
+    """,
+        """
+      update raw.graphql__teams_headcount
+      set loaded_to_conformed = true
+    """,
+    ],
+)
+primary = PythonOperator(task_id="raw.graphql__teams_headcount", python_callable=main, dag=dag)
+
+latest_only >> create_tables >> primary >> trasform_to_conformed
---
+++ dags/utils/gdriveutil.py
@@ -0,0 +1,96 @@
+import numpy as np
+from oauth2client.service_account import ServiceAccountCredentials
+from airflow.hooks.base_hook import BaseHook
+import json
+import io
+import googleapiclient
+from googleapiclient.discovery import build
+from httplib2 import Http
+import logging
+import pytz
+from datetime import datetime, timezone
+
+GOOGLE_SERVICE_ACCOUNT_CREDENTIALS = BaseHook.get_connection("aap_google_service_account")
+
+
+class GDriveUtil:
+    def __init__(self):
+        self.scope = [
+            "https://www.googleapis.com/auth/drive",
+        ]
+        self.credentials = ServiceAccountCredentials.from_json_keyfile_dict(
+            json.loads(
+                json.loads(GOOGLE_SERVICE_ACCOUNT_CREDENTIALS.extra)[
+                    "extra__google_cloud_platform__keyfile_dict"
+                ]
+            ),
+            self.scope,
+        )
+
+        self.http_auth = self.credentials.authorize(Http())
+        # print("self.http_auth:" + str(self.http_auth))
+        # dir_ = dir(self.http_auth)
+        # for att in dir_:
+        #     attr = getattr(self.http_auth, att)
+        #     print(f"att: {att} - {attr}")
+        #     _dir_ = dir(attr)
+        #     for _att in _dir_:
+        #         _attr = getattr(attr, _att)
+        #         print(f"_att: {_att} - {_attr}")
+        self.access_token = self.credentials.get_access_token(Http()).access_token
+        # print("self.access_token:" + str(access_token))
+        # dir_ = dir(access_token)
+        # for att in dir_:
+        #     attr = getattr(access_token, att)
+        #     print(f"att: {att} - {attr}")
+        #     _dir_ = dir(attr)
+        #     for _att in _dir_:
+        #         _attr = getattr(attr, _att)
+        #         print(f"_att: {_att} - {_attr}")
+        self.drive = build("drive", "v3", http=self.http_auth, cache_discovery=False)
+
+    def get_file_metadata(self, file_id=None, file_name=None):
+        if file_name:
+            res = self.drive.files().list().execute()
+            print("res" + str(res))
+            return [fi for fi in res["files"] if fi["name"] == file_name][0]
+        else:
+            return self.drive.files().get(fileId=file_id, supportsAllDrives=True).execute()
+        # return self.drive.files().list().execute()
+
+    def get_file_as_stream(self, file_id):
+        request = self.drive.files().get_media(fileId=file_id, supportsAllDrives=True)
+        f = io.BytesIO()
+        media_request = googleapiclient.http.MediaIoBaseDownload(f, request)
+
+        done = False
+        while done is False:
+            status, done = media_request.next_chunk()
+            logging.info("Download %d%%." % int(status.progress() * 100))
+        f.seek(0)
+        return f
+
+    def get_revision_id(self, file_id, time_of_revision, revision_list_tz=pytz.utc):
+        res = self.drive.revisions().list(fileId=file_id,).execute()
+
+        fmt_in = "%Y-%m-%dT%H:%M:%S.%fZ"
+        fmt_out = "%Y-%m-%d %H:%M:00"
+
+        for revision in res["revisions"]:
+            ts = datetime.strptime(revision["modifiedTime"], fmt_in).replace(tzinfo=pytz.UTC)
+            minute_trunc = datetime.strftime(ts.astimezone(revision_list_tz), fmt_out)
+            print(
+                "Revision:"
+                + str(revision)
+                + f": minute_trunc - {minute_trunc}: tor - {time_of_revision}"
+            )
+
+            if minute_trunc == time_of_revision:
+                return revision["id"]
+
+    def get_file_id(self, fi_name):
+        res = self.drive.files().list().execute()
+        return [fi["id"] for fi in res["files"] if fi["name"] == fi_name][0]
+
+    def get_access_token(self):
+        return self.access_token
